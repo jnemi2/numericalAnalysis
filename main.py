ce52d6d17072e7743c304f4724aa81f146c21444
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-10-04 20:58:32 -0300

    ADD - Jacobi and Gauss-Seidel

---
+++ main.py
@@ -0,0 +1,56 @@
+from sympy import *
+
+
+x = symbols('x')
+y = symbols('y')
+z = symbols('z')
+
+exp = ["(1+2*y)/-3", "(15-x)/-4"]
+
+
+def systems_jacobi(expressions, symbols_vector, start_vector, accuracy=10, break_after=200):
+    expressions = sympify(expressions)
+    vector = list(start_vector)
+    prev_vector = None
+    operation_counter = 0
+    while prev_vector != vector:
+        print(operation_counter, vector)
+        if operation_counter >= break_after:
+            print("Maximum operations exceeded!")
+            break
+        prev_vector = list(vector)
+        for i in range(len(vector)):
+            this_exp = expressions[i]
+            for sym, val in zip(symbols_vector, list(prev_vector)):
+                this_exp = this_exp.subs(sym, val)
+            vector[i] = N(this_exp, accuracy)
+        operation_counter += 1
+    return vector
+
+
+def systems_gauss_seidel(expressions, symbols_vector, start_vector, accuracy=10, break_after=200):
+    expressions = sympify(expressions)
+    vector = list(start_vector)
+    prev_vector = None
+    operation_counter = 0
+    while prev_vector != vector:
+        print(operation_counter, vector)
+        if operation_counter >= break_after:
+            print("Maximum operations exceeded!")
+            break
+        prev_vector = list(vector)
+        for i in range(len(vector) - 1):
+            this_exp = expressions[i]
+            for sym, val in zip(symbols_vector, prev_vector):
+                this_exp = this_exp.subs(sym, val)
+            vector[i] = N(this_exp, accuracy)
+        this_exp = expressions[len(vector) - 1]
+        for i in range(len(vector) - 1):
+            this_exp = this_exp.subs(symbols_vector[i], vector[i])
+        vector[len(vector) - 1] = N(this_exp, accuracy)
+        operation_counter += 1
+    return vector
+
+
+systems_jacobi(exp, (x, y), (0, 0), accuracy=5)
+systems_gauss_seidel(exp, (x, y), (0, 0), accuracy=5)
