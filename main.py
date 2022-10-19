27e5950576218117e670b593af4b11115feff69e
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-10-19 19:29:53 -0300

    MOD - breaks_after & print format

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ f = sympify("x**5+x**4-17*x**2+15*x-1")
 g = sympify("sqrt(3*E**(-x))")
 
 
-def newton_raphson(func, start, accu):
+def newton_raphson(func, start, accu, break_after=200):
+    op_counter = 0
     deriv = diff(func, x)
     x0 = start
     xn = N(x0 - (func.subs(x, x0)/deriv.subs(x, x0)), accu)
-    print(f"valor: {xn}")
+    print("op:", op_counter, "valor:", xn)
     while str(xn) != str(x0):
+        if op_counter >= break_after:
+            print("Maximum operations exceeded!")
+            break
+        op_counter += 1
         x0 = xn
         xn = N(x0 - (func.subs(x, x0)/deriv.subs(x, x0)), accu)
-        print(f"valor: {xn}")
+        print("op:", op_counter, "valor:", xn)
     return xn
 
 
@@ -25,32 +30,35 @@ def punto_fijo(g, start, iters, accu=5):
     xn = start
     for i in range(iters):
         xn = N(g.subs(x, xn), accu)
-        print(f"valor: {xn}")
+        print("op:", i, "valor:", xn)
     return xn
 
 
-def bolzano(func, start, end, accu=5):
+def bolzano(func, start, end, accu=5, break_after=200):
+    op_counter = 1
     a = start
     b = end
     c = None
-    y = Symbol('y')
     average = (x+y)/2
     prev_c = ''
-    if (N(func.subs(x, b), accu)*N(func.subs(x, a), accu) < 0):
-        while (c != prev_c):
-            #print(a, b, a!=b)
+    if N(func.subs(x, b), accu)*N(func.subs(x, a), accu) < 0:
+        while c != prev_c:
+            if op_counter >= break_after:
+                print("Maximum operations exceeded!")
+                break
             prev_c = c
             c = N(average.subs(x, a).subs(y, b), accu)
-            print(c)
+            print("op:", op_counter, "valor:", c)
             temp = N(func.subs(x, a), accu)*N(func.subs(x, c), accu)
-            if (temp < 0):
+            if temp < 0:
                 b = c
-            elif (temp > 0):
+            elif temp > 0:
                 a = c
             else:
                 break
+            op_counter += 1
     else:
-        print(f"No sign change detected between f({start}) and f({end})")
+        print("No sign change detected between f(", start, ") and f(", end, ")")
     return c
 
 
