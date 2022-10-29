f35c7fe43a3ff825eb779a4a883942d4a1864a60
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-10-29 16:29:27 -0300

    MOD - integrating in main

--- main.py
+++ main.py
@@ -219,6 +219,11 @@ print(cn.trapecios(f, 0.0000000001, 1, h=0.1))
 """
 
 # E3
-
+"""
 exp = ["(4+y)/302", "(2-2*x)/6"]
 cn.systems_gauss_seidel(exp, (x, y), (0.013, 0.33), accu=6)
+"""
+
+f = sympify("(sin(0.02*x))/x")
+print(cn.trapecios(f, 0.0000000001, 1, h=0.1), "<- trapecios")
+print(cn.simpson(f, 0.0000000001, 1, h=0.1), "<- simpson")
